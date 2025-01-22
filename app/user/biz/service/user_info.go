ade0ccaddf1bc841e5c3a50e247acbf8e5ba6f0d
kyzyc <yuchenzhu66@gmail.com>
2025-01-22 18:20:40 +0800

    fix: correct mysql dal package.

--- app/user/biz/service/user_info.go
+++ app/user/biz/service/user_info.go
@@ -2,7 +2,7 @@ package service
 
 import (
 	"context"
-	"github.com/Vigor-Team/youthcamp-2025-mall-be/app/cart/biz/dal/mysql"
+	"github.com/Vigor-Team/youthcamp-2025-mall-be/app/user/biz/dal/mysql"
 	"github.com/Vigor-Team/youthcamp-2025-mall-be/app/user/biz/model"
 	user "github.com/Vigor-Team/youthcamp-2025-mall-be/rpc_gen/kitex_gen/user"
 )
@@ -16,8 +16,8 @@ func NewUserInfoService(ctx context.Context) *UserInfoService {
 
 // Run create note info
 func (s *UserInfoService) Run(req *user.UserInfoReq) (resp *user.UserInfoResp, err error) {
-	userId := req.UserId
-	userRow, err := model.GetByID(mysql.DB, s.ctx, uint(userId))
+	//userId := req.UserId
+	userRow, err := model.GetByID(mysql.DB, s.ctx, uint(req.UserId))
 	if err != nil {
 		return
 	}
